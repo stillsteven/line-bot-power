8fcfeba93298423a6c66a99d2625c6cef6833e35
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2017-12-16 22:51:39 +0800

    Update vodkabot.py

--- vodkabot.py
+++ vodkabot.py
@@ -8,7 +8,8 @@ import time, datetime, random ,sys, re, string, os, json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-client = LineClient(token="EnoDCEeDV8mKqeDrrjk7.Ez+ge4xnb0Ba7QMN9CAfHW.VuqHDbtKBk//GvYkz5PZ70JEbdz7Ul7DAD0isff5thg=")
+client = LineClient()
+client._qrLogin("line://au/q/")
 
 profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
 offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
